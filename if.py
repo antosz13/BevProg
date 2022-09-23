974bfde3f49b7a5f3d45ca2cc6a90dc602be1aee
antosz13 <113166630+antosz13@users.noreply.github.com>
2022-09-23 08:06:52 +0200

    Add files via upload

---
+++ if.py
@@ -0,0 +1,18 @@
+def main():
+    
+    age = int(input("Kor: "))
+    
+    if age >= 18:
+        print("igyal mert kulonben nem birod")
+        
+    elif age >= 12:
+        print("mar majdnem teso")
+    
+    else:
+        pass
+        
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ lista.py
@@ -0,0 +1,14 @@
+def main():
+    li = ['alma', 'korte', 'barack']
+    li.append('mango')
+    li.copy(li2)
+    utolso = li.pop() #utolso elemet kiirja
+    li.sort()
+    sorted()
+    li.index('barack')
+             
+            
+
+    
+    if __name__ == "__main__":
+        main()
